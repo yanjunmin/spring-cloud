849d83cc9ca958693bb82942b3459a340f0ccb7f
yanjunmin <1519484516@qq.com>
2019-02-27 21:15:19 +0800

    add

---
+++ eureka-client/src/main/java/top/westyle/eureka/client/EurekaClientApplication.java
@@ -0,0 +1,27 @@
+package top.westyle.eureka.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableEurekaClient
+@SpringBootApplication
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+
+    @Value("${server.port}")
+    private String port;
+
+    @RequestMapping("/hi")
+    public String home(@RequestParam(value = "name", defaultValue = "yanjunmin") String name) {
+        return "hi " + name + " ,i am from port:" + port;
+    }
+}
